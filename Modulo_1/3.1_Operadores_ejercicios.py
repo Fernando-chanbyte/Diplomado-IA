899f30816a88121c4db11537a8d6863133077a13
fernando-chanbyte <luisfchancutz@gmail.com>
2024-06-22 14:55:51 -0600

    Se añadió ejercicios de Estructuras de Control de flujo

--- Modulo_1/3.1_Operadores_ejercicios.py
+++ Modulo_1/3.1_Operadores_ejercicios.py
@@ -6,17 +6,11 @@ Escribir un programa que solicite dos números y luego imprima:
     4. El cubo de la suma de los dos numeros
     5. El cociente de la división del primer número por el segundo
 """
-num1 = float(input("ingrese el primer número "))
-num2 = float(input("ingrese el segundo número "))
+num1 = float(input("ingrese el primer número: "))
+num2 = float(input("ingrese el segundo número: "))
 
-suma = num1 + num2
-resta = num1 - num2
-producto = num1 * num2
-cubo = suma**3
-cociente = num1/num2
-
-print("suma:", suma)
-print("resta:", resta)
-print("producto:", producto)
-print("cubo", cubo)
-print("cociente:", cociente)
+print("La suma de los dos números:", num1 + num2 )
+print("La resta del primer número menos el segundo:", num1 - num2 )
+print("El producto de los dos números:", num1 * num2 )
+print("El cubo de la suma de los dos numeros:", (num1 + num2)**3 )
+print("El cociente de la división del primer número por el segundo:", num1 / num2)
